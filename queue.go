074ab488b225d182c2436bd5e8a39885cd98cdf5
Vali Malinoiu <0x4139@gmail.com>
2016-10-19 17:11:44 +0300

    Adds options to create a simple queue and a concurent queue

--- queue.go
+++ queue.go
@@ -1,11 +1,19 @@
 package ava
 
-
+import "gopkg.in/redis.v5"
 
 type Queue struct {
-
+	client *redis.Client
 }
 
-func NewQueue() {
+func NewQueue(options Options) *Queue {
+	return &Queue{
+		client:newClient(options),
+	}
+}
 
+func NewFailOverQueue(options FailoverOptions) *Queue {
+	return &Queue{
+		client:newFailOverClient(options),
+	}
 }
